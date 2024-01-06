58a31c8eae8ad58e6f73a0f521d56f918671cb40
SyedHazarath1234 <syed.hazarath@una-brands.com>
2024-01-06 12:53:56 +0530

    first commit

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TopBanner from '../Components/TopBanner/TopBanner'
+
+function HomePage() {
+  return (
+    <>
+      <TopBanner />
+    </>
+  )
+}
+
+export default HomePage
